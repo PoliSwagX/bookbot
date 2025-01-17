001c95c32dfbd526df16d27fe431f00e0f2eb7d1
PoliSwagX <jarrodwilliams1995@gmail.com>
2025-01-17 17:12:55 +1100

    Word count added

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = wordcount(text)
+    chars_dict = charactercount(text)
     print(f"{num_words} words found in the document")
+    print(chars_dict)
 
 def get_book_text(book_path):
     with open(book_path) as f:
@@ -12,3 +14,16 @@ def get_book_text(book_path):
 def wordcount(text):
     words = text.split()
     return len(words)
+
+def charactercount(text):
+    characters = {}
+    for c in text:
+        low_characters = c.lower()
+        if low_characters in characters:
+            characters[low_characters] += 1
+        else:
+            characters[low_characters] = 1
+    return characters
+
+
+main()
